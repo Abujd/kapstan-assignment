d2d3dad28c5e79cf4f9dca9f11c46bf8cd812852
Abujd <55851034+Abujd@users.noreply.github.com>
2024-05-06 16:55:12 +0530

    Updated page

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,25 @@
-import HomePage from "./components/homepage/HomePage";
+"use client";
+import { useState } from "react";
 import Info from "./components/info/Info";
 import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(true);
+  const [selectedOption, setSelectedOption] = useState(null);
+
   return (
-   <div className="flex items-start w-screen">
-    <Sidebar />
-    <div className="w-[87%]">
-      <Navbar />
-      <div className="cursor-pointer"><Info /></div>
+    <div className="flex items-start w-screen h-full">
+      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+      <div
+        className={`${isOpen ? "w-[100%]" : "w-[87%]"}`}
+        style={{ height: "100%" }}
+      >
+        <Navbar selectedOption={selectedOption} setSelectedOption={setSelectedOption}/>
+        <div className="cursor-pointer">
+          <Info selectedOption={selectedOption}/>
+        </div>
+      </div>
     </div>
-   </div>
   );
 }
